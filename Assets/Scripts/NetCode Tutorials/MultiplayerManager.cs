93dddeff2f1c642c29689e2241dd930786e0b760
Gybran Valdivia <gybman7@gmail.com>
2024-03-01 14:21:41 -0800

    Fixed Player Syncing
    Host/Clients now sync and only control their own player

--- Assets/Scripts/NetCode Tutorials/MultiplayerManager.cs
+++ Assets/Scripts/NetCode Tutorials/MultiplayerManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using Unity.Netcode;
 using UnityEngine;
 
-public class HelloWorldManager : MonoBehaviour
+public class MultiplayerManager : MonoBehaviour
 {
     private void OnGUI()
     {
