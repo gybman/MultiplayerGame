bd769a3b2f571da2d127faffc9ea0582fe009f6e
Gybran Valdivia <gybman7@gmail.com>
2024-03-15 14:45:49 -0700

    fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -127,7 +127,7 @@ public class PlayerMovement : NetworkBehaviour
                     // Play the sliding sound
                     audioSource.Play();
                     sameSlide = true;
-                    PlayerSlideClipClientRpc(transform.position);
+                    PlayerSlideClipServerRpc(transform.position);
                 }
             }
         }
@@ -176,7 +176,7 @@ public class PlayerMovement : NetworkBehaviour
                 {
                     // Play the sliding sound
                     audioSource.Play();
-                    PlayerSlideClipClientRpc(transform.position);
+                    PlayerSlideClipServerRpc(transform.position);
                     sameSlide = true;
                 }
             }
